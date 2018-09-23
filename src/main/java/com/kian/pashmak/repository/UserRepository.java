d2a4d2bd6f65eec6c8b811b7c04f4ea3059fd85c
sedaghatbin <f.sedaghatbin@kian.capital>
2018-09-23 19:37:19 +0330

    delete vote

--- src/main/java/com/kian/pashmak/repository/UserRepository.java
+++ src/main/java/com/kian/pashmak/repository/UserRepository.java
@@ -36,11 +36,11 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findOneWithAuthoritiesById(Long id);
 
     @EntityGraph(attributePaths = "authorities")
-    @Cacheable(cacheNames = USERS_BY_LOGIN_CACHE)
+//    @Cacheable(cacheNames = USERS_BY_LOGIN_CACHE)
     Optional<User> findOneWithAuthoritiesByLogin(String login);
 
     @EntityGraph(attributePaths = "authorities")
-    @Cacheable(cacheNames = USERS_BY_EMAIL_CACHE)
+//    @Cacheable(cacheNames = USERS_BY_EMAIL_CACHE)
     Optional<User> findOneWithAuthoritiesByEmail(String email);
 
     Page<User> findAllByLoginNot(Pageable pageable, String login);
